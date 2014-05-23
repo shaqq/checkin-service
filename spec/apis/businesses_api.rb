f390c82c40f8aa8193d38f7e2ec48a9301f6f211
Shaker Islam <shakeratheonion@gmail.com>
2014-05-22 20:17:36 -0500

    getting rid of helloapi, putting in some boilerplate specs

---
+++ spec/apis/businesses_api.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+def app
+  ApplicationApi
+end
+
+describe BusinessesApi do
+  include Rack::Test::Methods
+
+end
---
+++ spec/apis/checkins_api_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+def app
+  ApplicationApi
+end
+
+describe CheckinsApi do
+  include Rack::Test::Methods
+
+end
