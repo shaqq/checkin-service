a3a4edacb306613b7bd44be7e43d36bf23a74e6c
Shaker Islam <shakeratheonion@gmail.com>
2014-06-01 18:49:54 -0500

    creating a many-to-many between businesses and users, since we have it

--- app/models/business.rb
+++ app/models/business.rb
@@ -2,4 +2,5 @@ class Business < ActiveRecord::Base
   include Napa::FilterByHash
 
   has_many :checkins
+  has_many :users, through: :checkins
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   has_many :checkins
+  has_many :businesses, through: :checkins
 
   validates_presence_of :name, :email
   validates_presence_of :password, on: :create
